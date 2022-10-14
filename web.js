32932ea70402f1c61887ca88ebdea23d414a555c
sbarashi <cks040425@gmail.com>
2022-10-14 23:20:48 +0900

    123

---
+++ web.js
@@ -0,0 +1,9 @@
+const app = require("express")();
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(require("express").static('public'));
+app.listen(8001, () => { console.log("listening on 8001") });
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
